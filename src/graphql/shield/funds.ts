e109311b77a57894c823124d65440e36e08efac1
ACAZUMAH <calebazumah9@gmail.com>
2025-10-14 11:00:14 +0000

    feat(update): transaction queries

--- src/graphql/shield/funds.ts
+++ src/graphql/shield/funds.ts
@@ -1,4 +1,14 @@
+import { and } from "graphql-shield";
+import { isAdmin, isAuthenticated } from "./general";
+
 export const fundsShield = {
-  Query: {},
-  Mutation: {},
+  Query: {
+    getFundById: isAuthenticated,
+    getFunds: isAuthenticated,
+  },
+  Mutation: {
+    createFund: and(isAuthenticated, isAdmin),
+    updateFund: and(isAuthenticated, isAdmin),
+    deleteFund: and(isAuthenticated, isAdmin),
+  },
 };

fa8c76e97a746f95c2923d9e70aae8c195f029fd
ACAZUMAH <calebazumah9@gmail.com>
2026-01-26 14:53:15 +0000

    feat(update): new update

--- src/graphql/shield/documents.ts
+++ src/graphql/shield/documents.ts
@@ -1,20 +1,13 @@
-import { and, rule } from "graphql-shield";
-import { GraphqlContext } from "src/common/interfaces";
-import { isAdvisor, isAuthenticated } from "./general";
-import { MutationDeleteDocumentArgs } from "src/common/interfaces/graphql";
-
-const canDeleteDocument = rule()((_, args: MutationDeleteDocumentArgs, ctx: GraphqlContext) => {
-    return Boolean(ctx.user && ctx.user._id === args?.data?.userId);
-});
+import { and } from 'graphql-shield';
+import { isAdvisor, isAuthenticated } from './general';
 
 export const documentsShield = {
-    Query: {
-        getUserDocuments: isAuthenticated,
-        getDocumentById: isAuthenticated,
-        getClientsDocuments: and(isAuthenticated, isAdvisor),
-    },
-    Mutation: {
-        createDocument: isAuthenticated,
-        deleteDocument: and(isAuthenticated, canDeleteDocument),
-    },
+  Query: {
+    getUserDocuments: isAuthenticated,
+    getDocumentById: isAuthenticated,
+    getClientsDocuments: and(isAuthenticated, isAdvisor),
+  },
+  Mutation: {
+    createDocument: isAuthenticated,
+  },
 };

99ac34de7bc458de1d37a40ba937ba9943d64f5c
ACAZUMAH <calebazumah9@gmail.com>
2026-01-30 16:19:33 +0000

    feat(update): new

--- src/graphql/lookups/typeDefs.ts
+++ src/graphql/lookups/typeDefs.ts
@@ -1,24 +1,24 @@
 export const lookupsTypeDefs = `#graphql
   type Bank {
-    bankId: String!
-    bankName: String!
-    bankCode: String!
+    bankId: String
+    bankName: String
+    bankCode: String
   }
 
   type Country {
-    countryCode: String!
-    countryName: String!
+    countryCode: String
+    countryName: String
   }
 
   type FileType {
-    fileTypeId: String!
-    fileTypeName: String!
-    extension: String!
+    fileTypeId: String
+    fileTypeName: String
+    extension: String
   }
 
   type Relation {
     relationshipId: String!
-    name: String!
+    name: String
     email: String
     phoneNumber: String
     idNumber: String
